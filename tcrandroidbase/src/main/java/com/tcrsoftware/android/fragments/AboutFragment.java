dfc63cc953ce65381d1edfd2ab09249f3246a3e5
sai_s <ssunkar@yagnasoft.com>
2016-07-31 12:01:45 -0700

    test 1

---
+++ tcrandroidbase/src/main/java/com/tcrsoftware/android/fragments/AboutFragment.java
@@ -0,0 +1,60 @@
+package com.tcrsoftware.android.fragments;
+
+
+import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.tcrsoftware.android.R;
+
+
+/* testing git */
+
+
+public class AboutFragment extends Activity {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public AboutFragment() {
+        // Required empty public constructor
+    }
+
+// TODO: Rename and change types and number of parameters
+ /*   public static AboutFragment newInstance(String param1, String param2) {
+        AboutFragment fragment = new AboutFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }*/
+    TextView version;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       setContentView(R.layout.fragment_about);
+        getActionBar().setTitle("About");
+
+        version = (TextView)findViewById(R.id.version);
+        try {
+            final PackageInfo packageInfo = this.getPackageManager()
+                    .getPackageInfo(this.getPackageName(), 0);
+            version.setText(packageInfo.versionName
+            );
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        }
+    }
+
+
+
